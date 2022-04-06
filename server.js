eaef35f962e57b9826ed7e4fbbf13259b03e898c
Ednei Ferreira <edneirferreira28@gmail.coma>
2022-04-05 22:08:02 -0300

    confirmar

---
+++ server.js
@@ -0,0 +1,13 @@
+const config = require("config")
+const exoress = require("express")
+
+console.log("Meu primeiro servidor")
+
+const app = exoress();
+
+let port = config.get("server.port");
+
+app.listen(port, function(){
+    console.log("Servidor iniciado e escutando na porta " +port)
+})
+
